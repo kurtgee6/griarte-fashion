1ff7b873f7e9d55ab5e765cefc75619a947e4394
krisselgriarte <krissel.griarte@gmail.com>
2017-10-16 18:28:44 -0400

    added card component from materialize

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Shop from "./pages/Shop.js";
 import QuickView from "./pages/QuickView.js";
-import Fashion from "./pages/Fashion.js";
+import FashionPage from "./pages/FashionPage.js";
 import FAQ from "./pages/FAQ.js";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart.js";
 import "./App.css";
@@ -13,13 +13,12 @@ import "./App.css";
 const App = () =>
   <Router>
     <div>
-      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/About" component={About} />
         <Route exact path="/Shop" component={Shop} />
         <Route exact path="/Shop/:id" component={QuickView} />
-        <Route exact path="/Fashion" component={Fashion} />
+        <Route exact path="/FashionPage" component={FashionPage} />
         <Route exact path="/FAQ" component={FAQ} />
         <Route exact path="/ShoppingCart" component={ShoppingCart} />
       </Switch>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../components/Nav/Navbar.js";
-// import ShoppingCart from "../components/ShoppingCart/ShoppingCart.js"
+import ShoppingCart from "../components/ShoppingCart/ShoppingCart.js"
 import AboutGriarte from "../components/AboutG/aboutGriarte.js";
-// import Header from "../components/Header/Header.js";
 
 const About = () =>
     <div>
       <AboutGriarte/>
+      <ShoppingCart/>
+      <Navbar/>
     </div>;
 export default About;
