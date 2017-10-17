6ab874c795ffd1b32435c072422c9f84e6759dec
Rach Griarte <rachelgriarte@hotmail.com>
2017-10-17 15:23:27 -0400

    addeding details for each shirts

--- client/src/components/Nav/Navbar.js
+++ client/src/components/Nav/Navbar.js
@@ -8,13 +8,13 @@ const Navbar = () =>
       <Link to="/"><i class="material-icons" id="navIcons">home</i></Link>
     </li>
     <li className={window.location.pathname === "/advice" ? "active" : ""}>
-      <Link to="/Fashion"><i class="material-icons" id="navIcons">photo_camera</i></Link>
+      <Link to="/Shop"><i class="material-icons" id="navIcons">shopping_basket</i></Link>
     </li>
     <li className={window.location.pathname === "/advice" ? "active" : ""}>
       <Link to="/ShoppingCart"><i class="material-icons" id="navIcons">shopping_cart</i></Link>
     </li>
     <li className={window.location.pathname === "/advice" ? "active" : ""}>
-      <Link to="/Shop"><i class="material-icons" id="navIcons">shopping_basket</i></Link>
+      <Link to="/Fashion"><i class="material-icons" id="navIcons">photo_camera</i></Link>
     </li>
     <li className={window.location.pathname === "/advice" ? "active" : ""}>
       <Link to="/About"><i class="material-icons" id="navIcons">chrome_reader_mode</i></Link>
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Nav/Navbar.js";
 import ShopNavbar from "../components/Nav/ShopNavbar.js";
-import ScrollHorizontal from "../components/ScrollHorizontal/ScrollHorizontal.js";
 
 class Shop extends React.Component {
     constructor(props) {
@@ -10,10 +9,8 @@ class Shop extends React.Component {
     render() {
     return (
         <div>
-            <ScrollHorizontal>
             <Navbar/>
             <ShopNavbar/>
-            </ScrollHorizontal>
         </div>
     )};
 };
