f906e0e9cee8869b88a748f20c2be59f7e3ec08d
kurtgee6 <kurtgee11@gmail.com>
2017-10-18 09:35:33 -0400

    adding stripe testing

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-// const stripe = require('stripe')('sk_test_zEUsB2qIFm8kLWvwuCz9aI4D');
-
-// // Stripe
-
-// stripe.customers.create(
-//   { email: 'customer@example.com' },
-//   function(err, customer) {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       console.log(customer)
-//     }
-//   }
-// );
-
-
 //Require griarte fasion dependencies
 var express = require('express');
 var app = express();
@@ -22,6 +6,9 @@ var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var env = require('dotenv').load();
+
+//Stripe 
+const stripe = require('stripe')('sk_test_zEUsB2qIFm8kLWvwuCz9aI4D');
 
 //The code below allows our app to the body parser
 app.use(bodyParser.urlencoded({extended: true}));
@@ -34,32 +21,25 @@ app.use(passport.initialize());
  
 app.use(passport.session()); // persistent login sessions
 
- 
- 
 app.get('/', function(req, res) {
  
     res.send('Welcome to Griarte Fashion!');
  
 });
 
+
 //Models
 var models = require("./models");
  
 //Sync Database
 models.sequelize.sync({force: true}).then(function() {
- 
     console.log('Nice! Database looks fine')
- 
 }).catch(function(err) {
- 
     console.log(err, "Something went wrong with the Database Update!")
- 
 });
  
 app.listen(5000, function(err) {
- 
     if (!err)
         console.log("Site is live");
     else console.log(err)
- 
 });
