7c5c2d26bcc8419422cf70e585a6f15ab8d84b4c
Rach Griarte <rachelgriarte@hotmail.com>
2017-10-21 10:26:02 -0400

    don't pay attention to this

--- client/src/components/HomeStuff/homeStuff.js
+++ client/src/components/HomeStuff/homeStuff.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Tab, Tabs } from "react-materialize";
 import "./homeStuff.css";
 
 const HomeStuff = () =>
     <div>
         {/* About Griarte Image */}
-        {<img className="homeImage" alt="Home Image" src={require("../../Images/gigi-hadid3.jpg")} />}
+        {<img className="homeImage" alt="Home Image" src={require("../../griarte_files/gigi-hadid3.jpg")} />}
     </div>
 export default HomeStuff;
