21201d1f5f4203372c93fc8635c85d57c7d3e3e1
kurtgee6 <kurtgee11@gmail.com>
2017-11-01 12:22:56 -0400

    kurt update

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,26 @@ import Shop from "./pages/Shop.js";
 import QuickView from "./pages/QuickView.js";
 import FashionPage from "./pages/FashionPage.js";
 import FAQ from "./pages/FAQ.js";
-import SignUp from "./pages/SignUp.js";
-import SignIn from "./pages/SignIn.js";
 import Dummy from "./pages/dummypage.js";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart.js";
 import "./App.css";
 
 const App = () =>
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/About" component={About} />
-        <Route exact path="/Shop" component={Shop} />
-        <Route exact path="/Shop/:id" component={QuickView} />
-        <Route exact path="/Fashion" component={FashionPage} />
-        <Route exact path="/FAQ" component={FAQ} />
-        <Route exact path="/Dummy" component={Dummy} />
-        <Route exact path="/ShoppingCart" component={ShoppingCart} />
-        <Route exact path="/SignUp" component={SignUp} />
-        <Route exact path="/SignIn" component={SignIn} />
-      </Switch>
-    </div>
-  </Router>;
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/About" component={About} />
+              <Route exact path="/Shop" component={Shop}>
+              </Route>
+              <Route exact path="/Shop/:id" component={QuickView} />
+              <Route exact path="/Fashion" component={FashionPage} />
+              <Route exact path="/FAQ" component={FAQ} />
+              <Route exact path="/Dummy" component={Dummy} />
+              <Route exact path="/ShoppingCart" component={ShoppingCart} />
+            </Switch>
+          </div>
+        </Router>
+    ;
 
 export default App;
