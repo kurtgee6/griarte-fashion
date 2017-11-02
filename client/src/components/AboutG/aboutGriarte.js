9c3c7321dc06878d6dfd02fce78f6a1be137c62a
Rach Griarte <rachelgriarte@hotmail.com>
2017-11-01 20:58:32 -0400

    mobile responsive

--- client/src/components/AboutG/aboutGriarte.js
+++ client/src/components/AboutG/aboutGriarte.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Collapsible, CollapsibleItem, Slide, Slider} from 'react-materialize';
+import {Collapsible, CollapsibleItem, Slide, Slider, Button} from 'react-materialize';
 import "../../../node_modules/video-react/dist/video-react.css";
 import "./aboutGriarte.css";
 
@@ -18,7 +18,7 @@ const AboutG = (props) =>
             <p id="storyTimelineH1">History Timeline</p>
             <CollapsibleItem header='Beginning of Griarte'>
                 <p id="collapseHead"> 
-                    Kurt Griarte started <i>Griarte</i> in July of 2016. He sold hats and shirts to his friends in order to get the word out. 
+                    <i>Griarte</i> began in July of 2016 when Kurt Griarte sold hats and shirts to his friends in order to get the word out. There were only a total of 12 shirts and 12 hats in his first collection.
                 </p>
             </CollapsibleItem>
             <CollapsibleItem header='First Investor'>
@@ -28,41 +28,45 @@ const AboutG = (props) =>
             </CollapsibleItem>
             <CollapsibleItem header='Developing A Team' >
                 <p id="collapseHead"> 
-                    Kurt and Caleb developed <br/>a team in November.<br/><br/> 
-                    Rachel Griarte - Social Media Officer<br/>
-                    Grace Bailey - Customer Service Officer<br/>
-                    Sean Parekh - Chief Financial Officer<br/>
-                    Justin Hurdle - Lead Marketing Officer<br/>
-                    Alfaaz Mavani - ECU Marketing Officer
+                    Kurt and Caleb gathered a group of friends who are willing to work voluntarily due to their beliefs in Kurt's vision.<br/>
+                    [ Rachel Griarte - Social Media Officer ]<br/>
+                    [ Grace Bailey - Customer Service Officer ]<br/>
+                    [ Sean Parekh - Chief Financial Officer ]<br/>
+                    [ Justin Hurdle - Lead Marketing Officer ]<br/>
+                    [ Alfaaz Mavani - ECU Marketing Officer ]
                 </p>
             </CollapsibleItem>
                 <CollapsibleItem header='The First Launch'>
                    <p id="collapseHead"> 
-                       The <i>Griarte</i> website launched in December as well as the first collection. Things started happening quickly. Photoshoots were being scheduled, frats/sororities collaborations, and spreading the brand out.
+                       The <i>Griarte</i> website launched in December along with our first official clothing collection. Soon, the company began collaborating with frats/sororities, entrepreneurs, and manufacturers locally. Griarte also participated in voluntary photoshoots with people all over the Triangle. 
                     </p>
             </CollapsibleItem>
-                <CollapsibleItem header='Team Destruction' > 
-                    <p id="collapseHead"> 
-                        After the first launch, Caleb started a business structure which nobody liked, he started getting power hungry and asked Kurt to be a Co-CEO; however Kurt did not agree. Justin was the first member to quit the team followed by everybody else.
-                    </p>
-                </CollapsibleItem>
-                <CollapsibleItem header='Griarte Seperation'>
+            <CollapsibleItem header='The Official LLC'>
                 <p id="collapseHead"> 
-                    Due to the differences in their vision, Kurt left his compnay, then Griarte is trademarked by Caleb Smith.
+                    Before the end of the year, Caleb deceived Kurt into signing an agreement he wrote stating him to 50% ownership of the company. He then secretly passed an LLC soonafter without Kurt's permission.
                 </p>
-                </CollapsibleItem>
-                <CollapsibleItem header='The Story Of The Future'>
-                    <p id="collapseHead"> 
-                        Kurt Griarte will be rebranding <br/>in the near future.
-                    </p>
-                </CollapsibleItem>
-            </Collapsible>
-            </div>
+            </CollapsibleItem>
+            <CollapsibleItem header='Team Destruction' > 
+                <p id="collapseHead"> 
+                    After making the LLC, Caleb started a business structure that nobody in the team agreed upon. Many of the team members had trouble getting along with Caleb due to his dictorial behavior. Justin was the first member to quit the team followed by everyone else.
+                </p>
+            </CollapsibleItem>
+            <CollapsibleItem header='Griarte Seperation'>
+                <p id="collapseHead"> 
+                    Kurt and Caleb buttheaded when it came to decision making. Kurt wanted to be in charge of the company's direction, while Caleb wanted it to go his way. Due to the differences in their vision, Kurt left his company. Quickly after, Caleb trademarked Griarte.
+                </p>
+            </CollapsibleItem>
+            <CollapsibleItem header='The Story Of The Future'>
+                <p id="collapseHead"> 
+                <br/>Kurt Griarte will be rebranding <br/>in the near future.<br/><br/>
+                </p>
+            </CollapsibleItem>
+        </Collapsible>
+        </div>
         {/* The Team */}
         <div className="Team">
             <Slider>
                 <Slide
-                    src=""
                     placement="top">
                     <h3 id="TeamSlogan"> The Team</h3>
                     <h3 id="TeamSlogans">
@@ -70,7 +74,6 @@ const AboutG = (props) =>
                     </h3>
                 </Slide>
                 <Slide
-                    src=""
                     placement="left">
                     <h3 id="TeamSlogan"> Krissy Griarte<br/>Front-End Developer</h3>
                     <h3 id="TeamSlogans">
@@ -79,6 +82,9 @@ const AboutG = (props) =>
                         <br/>
                         <br/>Least Favorite: 
                         <br/>Debugging :(
+                        <a href="mailto:krissygriarte1992@gmail.com">
+                            <i id="yas" class="material-icons">email</i>                        
+                        </a>
                     </h3>
                 </Slide>
                 <Slide
@@ -90,10 +96,12 @@ const AboutG = (props) =>
                         <br/>
                         <br/>Least Favorite: 
                         <br/>Positoning/Resizing
+                        <a href="mailto:rachgriarte@griarte.com">
+                            <i id="yas" class="material-icons">email</i>                       
+                        </a>
                     </h3>
                 </Slide>
                 <Slide
-                    src=""
                     placement="left">
                     <h3 id="TeamSlogan"> Kurt Griarte<br/>Back-End Developer</h3>
                     <h3 id="TeamSlogans">
@@ -102,18 +110,23 @@ const AboutG = (props) =>
                         <br/>
                         <br/>Least Favorite: 
                         <br/>Setting up Stripe
+                        <a href="mailto:kurtgriarte@griarte.com">
+                            <i id="yas" class="material-icons">email</i>
+                        </a>
                     </h3>
                 </Slide>
                 <Slide
-                    src=""
                     placement="left">
                     <h3 id="TeamSlogan"> Melvin Hill<br/>Back-End Developer</h3>
                     <h3 id="TeamSlogans">
                         Favorite Part: 
-                        <br/>Designing the look and feel
+                        <br/>Functionality
                         <br/>
                         <br/>Least Favorite: 
-                        <br/>Positoning/Resizing
+                        <br/>Learning Redux!
+                        <a href="mailto:?">
+                            <i id="yas" class="material-icons">email</i>
+                        </a>
                     </h3>
                 </Slide>
             </Slider>
