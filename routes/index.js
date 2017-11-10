de07c0dfa1a56e47d973fce738caf11cb9369fd3
Kurt Griarte <kurtgee11@gmail.com>
2017-11-10 12:25:56 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-//Authentication Routes
-//router.use("/routes", authRoutes);
-
 // sending the React app
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
